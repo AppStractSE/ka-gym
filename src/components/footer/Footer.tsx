e2bd68c4e3ec521b4ed3b12ccae905a8905bd0ad
Alexander Auermaa <alexander.auermaa@gmail.com>
2024-02-26 00:11:29 +0100

    karl-alfred stamp added + footer fixes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,12 +1,14 @@
+import Image from "next/image";
 import Link from "next/link";
+import stampPic from "public/popeye.png";
 import { HiOutlineExternalLink } from "react-icons/hi";
 import { footerData } from "./data";
 
 const Footer = () => {
   return (
-    <footer className="w-full px-4 text-black border-t shadow-md shadow-t bg-vanilla-powder-500">
-      <div className="py-12 mx-auto max-w-8xl">
-        <div className="flex justify-between">
+    <footer className="shadow-t w-full border-t bg-vanilla-powder-500 px-4 text-black shadow-md">
+      <div className="mx-auto max-w-8xl py-12">
+        <div className="flex justify-between md:items-end">
           <div className="grid grid-cols-1 gap-12 md:grid-cols-2 md:gap-6">
             {footerData.map((link) => (
               <div key={link.label}>
@@ -43,23 +45,24 @@ const Footer = () => {
               </div>
             ))}
           </div>
-          <div className="space-y-8">
-            <Link href="/">
-              <img
-                src="/popeye.png"
-                alt="Karl Alfred Gym"
-                className="w-40 h-auto"
-              />
-            </Link>
-          </div>
+          <Link href="/" className="w-2/6 md:w-1/6">
+            <Image
+              alt="Karl-Alfreds Gym stämpellogga"
+              src={stampPic}
+              sizes="100vw"
+              style={{
+                width: "100%",
+                height: "auto",
+              }}
+            />
+          </Link>
         </div>
-
         <div className="my-6 h-[1px] w-full bg-black opacity-25 sm:mx-auto lg:my-8" />
         <div className="flex items-center justify-between">
           <a
             href="https://appstract.se/"
             target="_blank"
-            className="flex items-center gap-1 text-xs text-black opacity-50 w-fit hover:underline sm:text-center"
+            className="flex w-fit items-center gap-1 text-xs text-black opacity-50 hover:underline sm:text-center"
           >
             Webbdesign av appstract © 2024
           </a>
